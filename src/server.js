eced2257a57cd791d1b560fa8a39d7b6912ba49a
prebird <78974381+prebird@users.noreply.github.com>
2024-01-23 21:56:21 +0900

    chore: 웹 프레임워크인 express 추가

--- src/server.js
+++ src/server.js
@@ -1,1 +1,6 @@
-console.log("hello");
+import express from 'express';
+
+const app = express();
+
+const handleListen = () => console.log("Listening on http://localhost:3000");
+app.listen(3000, handleListen);
