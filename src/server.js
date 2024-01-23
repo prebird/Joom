5ea4d9a8c85922c261845816eea8176e83f348c7
prebird <78974381+prebird@users.noreply.github.com>
2024-01-23 22:14:12 +0900

    feat: 뷰 템플릿 엔진인 pug 로 뷰 렌더링

--- src/server.js
+++ src/server.js
@@ -2,5 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.set("view engine", "pug")
+app.set("views", __dirname + "/views");
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => console.log("Listening on http://localhost:3000");
 app.listen(3000, handleListen);
