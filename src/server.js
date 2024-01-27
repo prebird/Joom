b881db7f95baef786fa4cabb0cb1f15693652b8f
prebird <78974381+prebird@users.noreply.github.com>
2024-01-27 12:46:12 +0900

    feat: ejs 추가 및 static 설정 추가

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ import express from 'express';
 
 const app = express();
 
-app.set("view engine", "pug")
+app.set("view engine", "ejs")
 app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));  // 추가해주기
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (req, res) => {
+    res.render("home")
+});
+//app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 app.listen(3000, handleListen);
