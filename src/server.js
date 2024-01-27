fc1d8d58e1f8cb1dc29c92cdfe144801ce2d7124
prebird <78974381+prebird@users.noreply.github.com>
2024-01-27 13:03:03 +0900

    feat: websocket 추가 (ws 라이브러리)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
-const app = express();
+const app = express(); // node -> http 프로토콜을 사용하는 서버
 
 app.set("view engine", "ejs")
 app.set("views", __dirname + "/views");
@@ -9,7 +11,13 @@ app.use("/public", express.static(__dirname + "/public"));  // 추가해주기
 app.get("/", (req, res) => {
     res.render("home")
 });
-//app.get("/*", (req, res) => res.redirect("/"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const server = http.createServer(app);
+// webSocket 프로토콜을 사용하는 서버
+// http 서버를 파라메터로 넣어주었지만, 필수는 아닙니다.
+// 동일 포트에서 두 프로토콜을 같이 제공하기 위한 것입니다.
+const wss = new WebSocket.Server({ server });
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+server.listen(3000, handleListen);
