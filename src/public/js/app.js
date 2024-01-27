1edec95865810b819663a2d50dbec9348b6b7ce8
prebird <78974381+prebird@users.noreply.github.com>
2024-01-27 13:29:13 +0900

    feat: 클라이언트쪽 이벤트 추가 및 서버에서 메세지 전달

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,16 @@
+// 클라이언트
 const socket = new WebSocket(`ws://${window.location.host}`);  // 컴퓨터에서 - ws://localhost:3000 
+
+// 소켓 이벤트 핸들러 추가
+socket.addEventListener("open", (socket) => {
+    console.log("Conneted to Server");
+})
+
+socket.addEventListener("message", (socket) => {
+    console.log("message arrived");
+    console.log(socket.data);
+})
+
+socket.addEventListener("close", (socket) => {
+    console.log("Disconnected From Server");
+})
