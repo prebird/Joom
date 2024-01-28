8b7994135b3c7b1596531878927efd7259d571e9
prebird <78974381+prebird@users.noreply.github.com>
2024-01-28 12:58:20 +0900

    feat: 소켓 io로 변경

--- src/server.js
+++ src/server.js
@@ -3,8 +3,22 @@ import http from 'http';
 import SocketIO from 'socket.io'
 
 const app = express();
+
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));  // 추가해주기
+
+app.get("/", (req, res) => {
+    res.render("home")
+});
+app.get("/*", (req, res) => res.redirect("/"));
+
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+    console.log(socket);
+});
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
